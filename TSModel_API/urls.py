7e649af1aa7b1f836ac2487fc7cb6252ac17471a
Sumaiyah Chinoy <sumaiyahchinoy@gmail.com>
2023-05-20 17:15:13 +0500

    Revered back urls

--- TSModel_API/urls.py
+++ TSModel_API/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-# from .views import PatientIncomingPrediction
+from .views import PatientIncomingPrediction
 from .views2 import PatientIncomingData
 
 urlpatterns = [
-    # path('patientForecast/', PatientIncomingPrediction.as_view(), name = 'weight_prediction'),
-    #path('patientForecast/', PatientIncomingPrediction.as_view('get_model_predictions'), name='model_predictions'),
+    path('patientForecast/', PatientIncomingPrediction.as_view(), name = 'weight_prediction'),
     path('external-data/', PatientIncomingData.as_view(), name='external_data'),
-    # path('external-data/', PatientIncomingPrediction.as_view('get_data_from_external_file'), name='external_data'),
 ]
