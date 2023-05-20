672e848bdfe7fd9576353a55c634213217be805f
Sumaiyah Chinoy <sumaiyahchinoy@gmail.com>
2023-05-20 15:04:36 +0500

    removed ts model api for a while

--- TSModel_API/views.py
+++ TSModel_API/views.py
@@ -22,16 +22,16 @@ class PatientIncomingPrediction(APIView):
         # weight_predicted = np.round(weight_predicted, 1)
         # response_dict = {"Predicted Weight (kg)": weight_predicted}
 
-        ts_model = TSModelApiConfig.model
-        y_pred = ts_model.get_forecast(steps = 31)
-        y_pred_df = y_pred.conf_int(alpha = 0.05) 
-        y_pred_df["Predictions"] = ts_model.predict(start = y_pred_df.index[0], end = y_pred_df.index[-1])
-        y_pred_out = y_pred_df["Predictions"]
-        print(y_pred_out)
-        output_df = pd.DataFrame(y_pred_out)
-        output_df.reset_index(inplace=True)
-        print(output_df)
-        return Response(output_df, status=200) 
+        # ts_model = TSModelApiConfig.model
+        # y_pred = ts_model.get_forecast(steps = 31)
+        # y_pred_df = y_pred.conf_int(alpha = 0.05) 
+        # y_pred_df["Predictions"] = ts_model.predict(start = y_pred_df.index[0], end = y_pred_df.index[-1])
+        # y_pred_out = y_pred_df["Predictions"]
+        # print(y_pred_out)
+        # output_df = pd.DataFrame(y_pred_out)
+        # output_df.reset_index(inplace=True)
+        # print(output_df)
+        return Response("Success", status=200) 
     
     def get_data_from_external_file(self, request):
         data = get_time_series_data()  # Call the function from your Python file
