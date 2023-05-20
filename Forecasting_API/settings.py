5fcc5087cb135c682b7a2d548e407c857d530682
Sumaiyah Chinoy <sumaiyahchinoy@gmail.com>
2023-05-20 13:55:19 +0500

    Modified vercel.json again and settings.py

--- Forecasting_API/settings.py
+++ Forecasting_API/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -80,13 +80,14 @@ WSGI_APPLICATION = 'Forecasting_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {} # Prevent Django from loading an adapter
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
